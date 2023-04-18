cad6cafea8c45794c054dee7ea0c7244ac7a9157
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-18 10:28:32 -0300

    feat: add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
+import { env } from '@/env'
 import { mealsRoutes } from '@/routes'
 
 import fastify from 'fastify'
+import { StatusCodes } from 'http-status-codes'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(mealsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(StatusCodes.BAD_REQUEST)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
